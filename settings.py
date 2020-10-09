ac6f6bfc75d903d32ac23a12acddf4954de669be
Natalia Pipas <nataliapipas@vonq.com>
2020-10-09 15:54:24 +0200

    [PBK-51] Add health endpoint

--- settings.py
+++ settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'health_check',  # required
+    'health_check.db',  # stock Django health checkers
 ]
 
 MIDDLEWARE = [
