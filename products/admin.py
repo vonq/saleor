f58ddbf8e134eb5cbdda9077a302f052a366ccb0
Natalia Pipas <nataliapipas@vonq.com>
2020-10-13 15:35:30 +0200

    [PKB-7] Register channel and update local db name

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.products.models import Product
+from api.products.models import Product, Channel
 
 
 @admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Channel)
 class ProductAdmin(admin.ModelAdmin):
     pass
