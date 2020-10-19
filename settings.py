6db98271b2f3d804c7f1b121262a163bca8e5c0f
Natalia Pipas <nataliapipas@gmail.com>
2020-10-19 14:48:21 +0200

    [PKB-76] Add secret key (#24)
    Co-authored-by: Natalia Pipas <nataliapipas@vonq.com>

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z+j#(4au++@zc_)99@z$ouf+e^f4n5*6i^khjjav^_jn8u!m2y'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if is_development else False
