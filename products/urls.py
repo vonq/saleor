53e4f972def63ed796d0bb39c5f22f5281739211
marianmazarovici <marian@vonq.com>
2020-10-19 14:54:27 +0100

    Reformat products urls file

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from api.products.views import LocationSearchViewSet, ProductsViewSet
+from api.products.views import LocationSearchViewSet, ProductsListView
 
 app_name = "api.products"
 
 router = DefaultRouter()
 router.register(r"locations", LocationSearchViewSet, basename="locations")
-router.register(r"", ProductsViewSet, basename="")
-urlpatterns = router.urls
+urlpatterns = [
+    path(r"", ProductsListView.as_view(), name='products'),
+    path(r"locations", LocationSearchViewSet, name='locations')
+]
