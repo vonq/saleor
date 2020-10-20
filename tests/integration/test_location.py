47366589b89707266ce773cf32caa239463a3763
Natalia Pipas <nataliapipas@vonq.com>
2020-10-20 17:03:45 +0200

    [PKB-64] Use locations instead of locations-list

--- tests/integration/test_location.py
+++ tests/integration/test_location.py
@@ -4,16 +4,16 @@ from rest_framework.test import RequestsClient
 
 from api.products.serializers import LocationSerializer
 
+
 @tag('integration')
 class LocationsTestCase(TestCase):
     def test_locations_api_returns_200(self):
         client = RequestsClient()
-        response = client.get("http://testserver" + reverse("api.products:locations-list") + "?text=georgia")
+        response = client.get("http://testserver" + reverse("api.products:locations") + "?text=georgia")
         assert response.status_code == 200
 
     def test_locations_have_right_structure(self):
         client = RequestsClient()
-        response = client.get("http://testserver" + reverse("api.products:locations-list") + "?text=georgia")
+        response = client.get("http://testserver" + reverse("api.products:locations") + "?text=georgia")
         serializer = LocationSerializer(data=response.json()[0])
         assert serializer.is_valid() is True
-
