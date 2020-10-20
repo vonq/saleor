777c83645e18d8fc1c6516296cab5cfdb4fa18ff
Antonello D'Ippolito <antonellodippolito@gmail.com>
2020-10-20 13:54:18 +0200

    [PKB-64] Add integration test for locations autocomplete

---
+++ tests/integration/test_location.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, tag
+from rest_framework.reverse import reverse
+from rest_framework.test import RequestsClient
+
+from api.products.serializers import LocationSerializer
+
+@tag('integration')
+class LocationsTestCase(TestCase):
+    def test_locations_api_returns_200(self):
+        client = RequestsClient()
+        response = client.get("http://testserver" + reverse("api.products:locations-list") + "?text=georgia")
+        assert response.status_code == 200
+
+    def test_locations_have_right_structure(self):
+        client = RequestsClient()
+        response = client.get("http://testserver" + reverse("api.products:locations-list") + "?text=georgia")
+        serializer = LocationSerializer(data=response.json()[0])
+        assert serializer.is_valid() is True
+
