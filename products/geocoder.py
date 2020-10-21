b4c960a43961e4d4e098b2828ce9da37c8ab04bf
Antonello D'Ippolito <antonellodippolito@gmail.com>
2020-10-21 14:05:54 +0200

    [PKB-64] Fitered locations by type

--- products/geocoder.py
+++ products/geocoder.py
@@ -5,5 +5,5 @@ from mapbox import Geocoder as MapboxGeocoder
 class Geocoder:
     @staticmethod
     def geocode(text: str) -> List[Dict]:
-        response = MapboxGeocoder().forward(text)
+        response = MapboxGeocoder().forward(address=text, types=["country", "region", "place", "district"])
         return response.geojson()['features']
