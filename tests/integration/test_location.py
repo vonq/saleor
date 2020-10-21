7cee75d59c5a959ca2945a52dea3f613aabfc27c
Antonello D'Ippolito <antonellodippolito@gmail.com>
2020-10-21 13:47:53 +0200

    [PKB-64] Fix routes and tests client

--- tests/integration/test_location.py
+++ tests/integration/test_location.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, tag
 from rest_framework.reverse import reverse
-from rest_framework.test import RequestsClient
 
 from api.products.serializers import LocationSerializer
 
@@ -8,12 +7,10 @@ from api.products.serializers import LocationSerializer
 @tag('integration')
 class LocationsTestCase(TestCase):
     def test_locations_api_returns_200(self):
-        client = RequestsClient()
-        response = client.get("http://testserver" + reverse("api.products:locations") + "?text=georgia")
-        assert response.status_code == 200
+        response = self.client.get(reverse("api.products:locations") + "?text=georgia")
+        self.assertEqual(response.status_code, 200)
 
     def test_locations_have_right_structure(self):
-        client = RequestsClient()
-        response = client.get("http://testserver" + reverse("api.products:locations") + "?text=georgia")
-        serializer = LocationSerializer(data=response.json()[0])
-        assert serializer.is_valid() is True
+        response = self.client.get(reverse("api.products:locations") + "?text=georgia")
+        serializer = LocationSerializer(data=response.data[0])
+        self.assertTrue(serializer.is_valid())
