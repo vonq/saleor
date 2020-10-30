e350bd4506a628b8c0687d319cf0ba3b2344b444
Gabriele Alese <gabriele@alese.it>
2020-10-30 09:59:13 +0000

    Remove redundant url for the same viewset

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from api.products.views import LocationSearchViewSet, ProductsViewSet, IndustriesViewSet
+from api.products.views import ProductsViewSet, IndustriesViewSet
 
 app_name = "api.products"
 
 router = DefaultRouter()
-router.register(r"locations", LocationSearchViewSet, basename="locations")
 router.register(r"industries", IndustriesViewSet, basename="industries")
 router.register(r"", ProductsViewSet, basename="products")
 
