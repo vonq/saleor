32d5520d9566ca01af2da9488182fc8180c611a1
Gabriele Alese <gabriele@alese.it>
2020-11-04 18:58:18 +0100

    Fix Location serializer to expose an integer id

--- products/serializers.py
+++ products/serializers.py
@@ -4,7 +4,7 @@ from api.products.models import Product, Location, JobFunction, JobTitle, Indust
 
 
 class LocationSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(source='geocoder_id')
+    id = serializers.IntegerField()
     fully_qualified_place_name = serializers.CharField()
     canonical_name = serializers.CharField(allow_null=True)
     within = serializers.CharField(allow_null=True)
