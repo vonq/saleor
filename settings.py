e23112726bda383d86e00f2b9902c535be2f3cc9
Gabriele Alese <gabriele@alese.it>
2020-11-16 11:49:56 +0100

    Add Basic Auth support to PKB app (#105)

--- settings.py
+++ settings.py
@@ -97,6 +97,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ),
 }
 
