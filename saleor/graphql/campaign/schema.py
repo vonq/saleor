a169af7e19ca2e2b61b0a6be05fe8c0c41d55c17
Stanislav <s.rubtsov@sharksw.com>
2021-03-24 18:48:27 +0200

    Add look up query for campaign by id

--- saleor/graphql/campaign/schema.py
+++ saleor/graphql/campaign/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from ..core.fields import ChannelContextFilterConnectionField
+from ..channel import ChannelContext
 from .mutations.campaign import (
     CampaignCreate,
     CampaignUpdate,
@@ -18,9 +19,20 @@ class CampaignQuery(graphene.ObjectType):
         sort_by=CampaignSortInput(description="Sort campaigns."),
         filter=CampaignFilterInput(description="Filter campaigns.")
     )
+    campaign = graphene.Field(
+        CampaignType,
+        description="Look up a campaign by ID.",
+        id=graphene.Argument(
+            graphene.ID, description="ID of the campaign.", required=True
+        ),
+    )
 
     def resolve_campaigns(self, info, query=None, channel=None, **data):
         return resolve_campaings(info, query, channel_slug=None, **data)
+
+    def resolve_campaign(self, info, id, channel=None):
+        campaign = graphene.Node.get_node_from_global_id(info, id, CampaignType)
+        return ChannelContext(node=campaign, channel_slug=channel) if campaign else None
 
 
 class CampaignMutations(graphene.ObjectType):
