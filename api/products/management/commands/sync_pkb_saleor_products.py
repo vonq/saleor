1c2b89c87f2aea56e98f07d860799f1aea3b4b1e
marianmazarovici <marian@vonq.com>
2021-05-11 14:36:49 +0100

    Guard against pkb null values for required saleor fields

--- api/products/management/commands/sync_pkb_saleor_products.py
+++ api/products/management/commands/sync_pkb_saleor_products.py
@@ -34,12 +34,12 @@ class Command(BaseCommand):
         for pkb_product in pkb_products:
             saleor_product, _ = SaleorProduct.objects.get_or_create(
                 product_type=default_product_type,
-                name=pkb_product.title,
+                name=pkb_product.title or "Untitled",
                 slug=pkb_product.product_id
             )
             saleor_product_variant, _ = ProductVariant.objects.get_or_create(
                 id=pkb_product.id,  # IMPORTANT
-                name=pkb_products.title,
+                name=pkb_product.title or "Untitled",
                 sku=pkb_product.product_id,
                 product=saleor_product,
                 track_inventory=False
@@ -59,5 +59,5 @@ class Command(BaseCommand):
                 variant=saleor_product_variant,
                 channel=eur_channel,
                 currency="EUR",
-                price_amount=pkb_product.unit_price
+                price_amount=pkb_product.unit_price or 0
             )
