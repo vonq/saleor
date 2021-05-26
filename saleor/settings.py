e806a227c5fa8906fa74090287e1f6c4996373f8
Gabriele Alese <gabriele@alese.it>
2021-05-26 15:32:02 +0100

    Add stub for SF Contracts payment gateway

--- saleor/settings.py
+++ saleor/settings.py
@@ -564,6 +564,7 @@ PLUGINS = [
     "saleor.payment.gateways.authorize_net.plugin.AuthorizeNetGatewayPlugin",
     "saleor.plugins.invoicing.plugin.InvoicingPlugin",
     "api.plugins.checkout_details_plugin.CheckoutDetailsPlugin",
+    "api.plugins.contracts_payment_plugin.ContractsPaymentPlugin",
 ]
 
 # Plugin discovery
