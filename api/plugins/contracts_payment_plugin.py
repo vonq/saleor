4206934c6ab4559f3acde58cc2ef983204cfe13d
Gabriele Alese <gabriele@alese.it>
2021-06-02 08:39:13 +0100

    Display gateway options on checkout flow

--- api/plugins/contracts_payment_plugin.py
+++ api/plugins/contracts_payment_plugin.py
@@ -1,8 +1,17 @@
-from typing import List, Optional
+import json
+from typing import List, Optional, Any
 
 from saleor.payment.interface import GatewayResponse, PaymentData, CustomerSource, \
     PaymentGateway
-from saleor.plugins.base_plugin import BasePlugin
+from saleor.plugins.base_plugin import BasePlugin, PluginConfigurationType
+
+
+class ContractsRepository:
+    def get_contract_by_id(self):
+        pass
+
+    def charge_contract(self):
+        pass
 
 
 class ContractsPaymentPlugin(BasePlugin):
@@ -11,61 +20,37 @@ class ContractsPaymentPlugin(BasePlugin):
     PLUGIN_DESCRIPTION = ""
     CONFIG_STRUCTURE = None
     DEFAULT_CONFIGURATION = [
-            {"name": "Public API key", "value": None},
-            {"name": "Secret API key", "value": None},
-            {"name": "Store customers card", "value": False},
-            {"name": "Automatic payment capture", "value": True},
-            {"name": "Supported currencies", "value": "EUR,USD,GBP"},
-        ]
+        {"name": "Public API key", "value": None},
+        {"name": "Secret API key", "value": None},
+        {"name": "Store customers card", "value": False},
+        {"name": "Automatic payment capture", "value": True},
+        {"name": "Supported currencies", "value": "EUR,USD,GBP"},
+    ]
     DEFAULT_ACTIVE = False
 
     def get_client_token(self, token_config, previous_value):
         return previous_value
 
-    def authorize_payment(
-        self, payment_information: "PaymentData", previous_value
-    ) -> "GatewayResponse":
-        return previous_value
+    def get_payment_config(self, previous_value):
+        # this one returns a list of configuration after
+        # you call the createCheckout mutation
+        return [
+            {"field": "contract_1", "value": json.dumps({"balance": 10})},
+            {"field": "contract_2", "value": json.dumps({"balance": 20})},
+        ]
 
-    def refund_payment(
-        self, payment_information: "PaymentData", previous_value
-    ) -> "GatewayResponse":
-        return previous_value
-
-    def capture_payment(
-        self, payment_information: "PaymentData", previous_value
-    ) -> "GatewayResponse":
-        return previous_value
-
-    def void_payment(
-        self, payment_information: "PaymentData", previous_value
-    ) -> "GatewayResponse":
-        return previous_value
+    def token_is_required_as_payment_input(self, previous_value):
+        return False
 
     def process_payment(
-        self, payment_information: "PaymentData", previous_value
+            self, payment_information: "PaymentData", previous_value
     ) -> "GatewayResponse":
+        # all the logic goes here
+        # get the contract by id
+        # check the balance
+        # adjust the balance
+        # return status?
         return previous_value
 
     def get_supported_currencies(self, previous_value):
         return ["EUR", "GBP", "USD"]
-
-    def get_payment_gateway(
-        self, currency: Optional[str], previous_value
-    ) -> Optional["PaymentGateway"]:
-
-        return PaymentGateway(
-            id=self.PLUGIN_ID,
-            name=self.PLUGIN_NAME,
-            config=self.configuration,
-            currencies=self.get_supported_currencies(previous_value=[]),
-        )
-
-
-    def list_payment_sources(
-        self, customer_id: str, previous_value
-    ) -> List["CustomerSource"]:
-        return [
-            CustomerSource(id="1", gateway="boh"),
-            CustomerSource(id="2", gateway="boh"),
-        ]
