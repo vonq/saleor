60de7853d6df436d415936697e1e939d03c5a9ab
Gabriele Alese <gabriele@alese.it>
2021-06-03 17:28:59 +0100

    Implement draft contracts payments

--- api/plugins/contracts_payment_plugin.py
+++ api/plugins/contracts_payment_plugin.py
@@ -5,7 +5,6 @@ from saleor.payment.interface import GatewayResponse, PaymentData
 from saleor.plugins.base_plugin import BasePlugin
 
 
-
 @dataclass
 class Contract:
     customer_id: str
@@ -18,26 +17,40 @@ class Contract:
 class InsufficientFunds(Exception):
     pass
 
+
+class NonExistentContract(Exception):
+    pass
+
+
 class ContractsRepository:
 
     contracts = {
-        'customer_1': Contract('customer_1', 100),
-        'customer_2': Contract('customer_1', 100)
+        "contract_1": Contract("customer_1", 100),
+        "contract_2": Contract("customer_1", 100),
     }
 
     @classmethod
     def get_contract_by_customer_id(cls, customer_id):
         return cls.contracts.get(customer_id)
 
-
     @classmethod
     def charge_contract(cls, customer_id, billed_amount):
         contract = cls.get_contract_by_customer_id(customer_id)
+        print(contract)
+        print(contract.balance)
         if contract:
             if contract.can_charge(billed_amount):
                 contract.balance -= billed_amount
             else:
-                raise InsufficientFunds("Cannot charge contract because of insufficient funds")
+                raise InsufficientFunds(
+                    "Cannot charge contract because of insufficient funds"
+                )
+        else:
+            raise NonExistentContract
+
+    @classmethod
+    def get_all_contracts(cls):
+        return cls.contracts
 
 
 class ContractsPaymentPlugin(BasePlugin):
@@ -52,7 +65,7 @@ class ContractsPaymentPlugin(BasePlugin):
         {"name": "Automatic payment capture", "value": True},
         {"name": "Supported currencies", "value": "EUR,USD,GBP"},
     ]
-    DEFAULT_ACTIVE = False
+    DEFAULT_ACTIVE = True
 
     def get_client_token(self, token_config, previous_value):
         return previous_value
@@ -60,22 +73,52 @@ class ContractsPaymentPlugin(BasePlugin):
     def get_payment_config(self, previous_value):
         # this one returns a list of configuration after
         # you call the createCheckout mutation
+        contracts = ContractsRepository.get_all_contracts()
         return [
-            {"field": "contract_1", "value": json.dumps({"balance": 10})},
-            {"field": "contract_2", "value": json.dumps({"balance": 20})},
+            {
+                "field": contract.customer_id,
+                "value": json.dumps({"balance": contract.balance}),
+            }
+            for contract in contracts.values()
         ]
 
     def token_is_required_as_payment_input(self, previous_value):
         return False
 
     def process_payment(
-            self, payment_information: "PaymentData", previous_value
+        self, payment_information: "PaymentData", previous_value
     ) -> "GatewayResponse":
         # all the logic goes here
         # get the contract by id
         # check the balance
         # adjust the balance
         # return status?
+        token = payment_information.token
+        try:
+            ContractsRepository.charge_contract(
+                token, billed_amount=payment_information.amount
+            )
+        except InsufficientFunds as e:
+            return GatewayResponse(
+                is_success=False,
+                action_required=False,
+                kind="capture",
+                error="Insufficient funds!",
+                amount=payment_information.amount,
+                currency=payment_information.currency,
+                transaction_id="1234",
+            )
+        except NonExistentContract:
+            return GatewayResponse(
+                is_success=False,
+                action_required=False,
+                kind="capture",
+                error=f"No contract available for {token}",
+                amount=payment_information.amount,
+                currency=payment_information.currency,
+                transaction_id="1234",
+            )
+
         return GatewayResponse(
             is_success=True,
             action_required=False,
@@ -83,7 +126,7 @@ class ContractsPaymentPlugin(BasePlugin):
             error=None,
             amount=payment_information.amount,
             currency=payment_information.currency,
-            transaction_id='1234', # TODO
+            transaction_id="1234",  # TODO
             customer_id=payment_information.token,  # This is a workaround!
         )
 
