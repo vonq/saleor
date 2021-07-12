8724d74fafacfec7606134fd0178e032c327b326
Gabriele Alese <gabriele@alese.it>
2021-07-12 16:17:35 +0200

    External Auth0 authentication 2
    Implement verification and authentication

--- addons/plugins/auth0_plugin.py
+++ addons/plugins/auth0_plugin.py
@@ -18,6 +18,37 @@ from django.conf import settings
 
 User = get_user_model()
 
+JWKS_URL = f"https://{settings.AUTH0_DOMAIN}/.well-known/jwks.json"
+ISSUER = f"https://{settings.AUTH0_DOMAIN}/"
+
+
+def verify_token(token):
+    sv = AsymmetricSignatureVerifier(JWKS_URL)
+    tv = TokenVerifier(
+        signature_verifier=sv, issuer=ISSUER, audience=settings.AUTH0_CLIENT_ID
+    )
+
+    try:
+        tv.verify(token)
+    except TokenValidationError as e:
+        raise ValidationError(f"Invalid token! {e}")
+
+
+def get_user_from_token(token) -> Optional[User]:
+    user_id_token = jwt.decode(token, options={"verify_signature": False})
+    user_email = user_id_token["email"]
+    email_verified = user_id_token["email_verified"]
+
+    if not email_verified:
+        raise ValidationError("User email hasn'\t been verified yet.")
+
+    try:
+        user = User.objects.get(email=user_email)
+    except User.DoesNotExist:
+        raise ValidationError(f"Unknown user {user_email}")
+
+    return user
+
 
 class Auth0Plugin(BasePlugin):
     PLUGIN_NAME = "Authenticate with Auth0"
@@ -57,33 +88,9 @@ class Auth0Plugin(BasePlugin):
 
         payload = resp.json()
 
-        jwks_url = f"https://{settings.AUTH0_DOMAIN}/.well-known/jwks.json"
-        issuer = f"https://{settings.AUTH0_DOMAIN}/"
+        verify_token(payload["id_token"])
 
-        sv = AsymmetricSignatureVerifier(jwks_url)
-        tv = TokenVerifier(
-            signature_verifier=sv, issuer=issuer, audience=settings.AUTH0_CLIENT_ID
-        )
-
-        try:
-            tv.verify(payload["id_token"])
-        except TokenValidationError as e:
-            raise ValidationError(f"Invalid token! {e}")
-
-        user_id_token = jwt.decode(
-            payload["id_token"], options={"verify_signature": False}
-        )
-        user_email = user_id_token["email"]
-        email_verified = user_id_token["email_verified"]
-
-        if not email_verified:
-            raise ValidationError("User email hasn'\t been verified yet.")
-
-        try:
-            user = User.objects.get(email=user_email)
-        except User.DoesNotExist:
-            # TODO: implement a get_or_create!
-            raise ValidationError(f"Unknown user {user_email}")
+        user = get_user_from_token(payload["id_token"])
 
         return ExternalAccessTokens(
             token=payload["access_token"],
@@ -103,9 +110,18 @@ class Auth0Plugin(BasePlugin):
     def external_verify(
         self, data: dict, request: WSGIRequest, previous_value
     ) -> Tuple[Optional["User"], dict]:
-        pass
+        token = data["token"]
+
+        verify_token(token)
+        user = get_user_from_token(token)
+
+        return user, {}
 
     def authenticate_user(
         self, request: WSGIRequest, previous_value
     ) -> Optional["User"]:
-        pass
+        token = request.META.get("HTTP_AUTHORIZATION", " ").split(" ")[1]
+
+        user = get_user_from_token(token)
+
+        return user
